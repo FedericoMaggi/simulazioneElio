62ca0b1916a88167be9ba8689311c008c69b72bb
Federico <federico.maggipinto@studenti.unitn.it>
2019-01-14 09:37:22 +0100

    prova

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000
+ 
+function string_square(s){
+    if (typeof(s)==String){
+        res = length(s)*length(s);
+    } else {
+        res = -1;
+    }
+    return res;
+}
+
+app.get('/square?string=my_string', (req, res) => {
+    const s = req.query.my_string
+    string_square(s);
+})
+
+app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
